8b2aaa3ff4cd1eb87d5f2215c45c10aa7ce9c34b
MondeAnna <monde.anna@gmail.com>
2025-01-23 04:13:38 +0200

    workspace (setup): base code bootstaped via `spring`

---
+++ src/main/java/com/enviro/assessment/grad001/mondeanna/MondeAnnaApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.mondeanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondeAnnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondeAnnaApplication.class, args);
+	}
+
+}
