c474ac9e40883e72020c9a56fd1dcf0b25eabf81
MondeAnna <monde.anna@gmail.com>
2025-01-23 19:28:16 +0200

    waste (category): `get` -> add `findById` call

--- src/main/java/com/enviro/assessment/grad001/mondeanna/controller/CategoryController.java
+++ src/main/java/com/enviro/assessment/grad001/mondeanna/controller/CategoryController.java
@@ -3,12 +3,15 @@ package com.enviro.assessment.grad001.mondeanna.controller;
 import com.enviro.assessment.grad001.mondeanna.model.Category;
 import com.enviro.assessment.grad001.mondeanna.repository.CategoryRepository;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping( path = "/api/v1/categories" )
@@ -24,5 +27,10 @@ public class CategoryController {
     @GetMapping( path = "/" )
     public List<Category> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping( path = "/{id}" )
+    public Optional<Category> findById( @Valid @PathVariable long id ){
+        return repository.findById( id );
     }
 }
--- src/test/java/com/enviro/assessment/grad001/mondeanna/controller/CategoryControllerTest.java
+++ src/test/java/com/enviro/assessment/grad001/mondeanna/controller/CategoryControllerTest.java
@@ -13,6 +13,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class CategoryControllerTest {
@@ -29,5 +30,20 @@ public class CategoryControllerTest {
     public void testFindAll(){
         when( repository.findAll() ).thenReturn( mockRepo );
         assertThat( controller.findAll().size() ).isEqualTo( 2 );
+    }
+
+    @Test
+    public void testFindById(){
+        Category typeTwo = new Category(
+                2L,
+                "Type2",
+                """
+                leachable concentration between 1 and 2 as well as \
+                total concentration less that level 1 is type 2
+                """
+        );
+
+        when( repository.findById( 2L )).thenReturn( Optional.of( typeTwo ));
+        assertThat( controller.findById( 2L )).isEqualTo( Optional.of( TestData.typeTwo() ));
     }
 }
