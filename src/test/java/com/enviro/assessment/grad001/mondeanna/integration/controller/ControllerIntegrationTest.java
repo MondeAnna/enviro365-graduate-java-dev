038093208bd7022a498d0d2f67c4ace20f0dd593
MondeAnna <monde.anna@gmail.com>
2025-01-24 15:20:23 +0200

    integration (test): make `findAll` call

--- src/test/java/com/enviro/assessment/grad001/mondeanna/integration/controller/ControllerIntegrationTest.java
+++ src/test/java/com/enviro/assessment/grad001/mondeanna/integration/controller/ControllerIntegrationTest.java
@@ -1,13 +1,55 @@
 package com.enviro.assessment.grad001.mondeanna.integration.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.enviro.assessment.grad001.mondeanna.model.Category;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+@AutoConfigureMockMvc
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+@DirtiesContext( classMode = ClassMode.BEFORE_EACH_TEST_METHOD )
 public class ControllerIntegrationTest {
 
-    @Test
-    public void testContextLoads(){
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+    private final String requestMapping = "/api/v1/categories";
+
+    @Test
+    public void testIntegrationOfFindAll() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform( get( requestMapping + "/" ))
+                .andExpect( status().isOk() )
+                .andReturn().getResponse();
+
+        List<Category> responseRepository = deserializeRepository( response );
+
+        assertThat( responseRepository.get( 0 ).getId() ).isEqualTo( 1 );
+        assertThat( responseRepository.get( 1 ).getName() ).isEqualTo( "Type 1" );
+        assertThat( responseRepository.get( 2 ).getDescription() ).contains( "(LCT1 < LC <= LCT2 or TC <= TCT1)" );
+    }
+
+    private List<Category> deserializeRepository(MockHttpServletResponse response ) throws UnsupportedEncodingException, JsonProcessingException {
+        return objectMapper.readValue( response.getContentAsString(), new TypeReference<>(){} );
     }
 }
