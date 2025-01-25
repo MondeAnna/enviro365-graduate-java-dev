7d45bdb9b28fe91dd8fcb628d7b520ddc3454174
MondeAnna <monde.anna@gmail.com>
2025-01-25 17:07:39 +0200

    disposal (model): remove uniqueness trait for `description` and `landfill` columns

--- src/main/java/com/enviro/assessment/grad001/mondeanna/disposal/DisposalGuideline.java
+++ src/main/java/com/enviro/assessment/grad001/mondeanna/disposal/DisposalGuideline.java
@@ -33,10 +33,8 @@ public class DisposalGuideline {
         private String wasteCategory;
 
         @NotBlank
-        @Column( unique = true )
         private String description;
 
         @NotBlank
-        @Column( unique = true )
         private String landfill;
 }
