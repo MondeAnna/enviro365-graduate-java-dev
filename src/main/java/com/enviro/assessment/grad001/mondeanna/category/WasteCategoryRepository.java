f781ae771b5af66bf5289f9b94f40ded27d25ce0
MondeAnna <monde.anna@gmail.com>
2025-01-25 14:57:05 +0200

    disposal (repository): remove unused imports

--- src/main/java/com/enviro/assessment/grad001/mondeanna/category/WasteCategoryRepository.java
+++ src/main/java/com/enviro/assessment/grad001/mondeanna/category/WasteCategoryRepository.java
@@ -1,6 +1,5 @@
 package com.enviro.assessment.grad001.mondeanna.category;
 
-import com.enviro.assessment.grad001.mondeanna.disposal.DisposalGuideline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
