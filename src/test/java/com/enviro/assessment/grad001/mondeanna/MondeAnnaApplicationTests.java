f8d7c56e4f55493305b5021040c42b7e532689c3
MondeAnna <monde.anna@gmail.com>
2025-01-26 12:53:51 +0200

    package (mondeanna): return context loading test for `MondeAnnaApplication`

---
+++ src/test/java/com/enviro/assessment/grad001/mondeanna/MondeAnnaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.enviro.assessment.grad001.mondeanna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondeAnnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
