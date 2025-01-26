9d80690a3903f2f5788217490bdd0e0dc3698e6e
MondeAnna <monde.anna@gmail.com>
2025-01-26 09:53:30 +0200

    recycling (tip): ensure tip non-blankness is validated

--- src/test/java/com/enviro/assessment/grad001/mondeanna/recycling/unit/RecyclingTipTest.java
+++ src/test/java/com/enviro/assessment/grad001/mondeanna/recycling/unit/RecyclingTipTest.java
@@ -3,16 +3,31 @@ package com.enviro.assessment.grad001.mondeanna.recycling.unit;
 import com.enviro.assessment.grad001.mondeanna.recycling.TestData;
 import com.enviro.assessment.grad001.mondeanna.recycling.RecyclingTip;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class RecyclingTipTest {
 
+    private final String validationMessage = "must not be blank";
     private RecyclingTip tip;
+    private Validator validator;
+
+    @BeforeEach
+    public void setUp(){
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
 
     @Test
     public void testClassClassificationValue(){
@@ -22,5 +37,14 @@ public class RecyclingTipTest {
         assertThat( tip.getId() ).isEqualTo( 1 );
         assertThat( tip.getTip() ).isEqualTo( "Check Water Sources" );
         assertThat( tip.getDescription() ).isEqualTo( expectedDescription );
+    }
+
+    @Test
+    public void testInvalidTip(){
+        tip = TestData.invalidTip();
+
+        Set<ConstraintViolation<RecyclingTip>> violations = validator.validate( tip );
+        List<String> messages = violations.stream().map( ConstraintViolation::getMessage ).toList();
+        assertThat( messages.contains( validationMessage )).isTrue();
     }
 }
