bedea4731bd8b30fbd203f64d7fdf3ed94ed7a46
MondeAnna <monde.anna@gmail.com>
2025-01-26 12:08:40 +0200

    recycling (tests): `integration` -> ensure tip related tests are operational

--- src/test/java/com/enviro/assessment/grad001/mondeanna/recycling/integration/RecyclingTipIntegrationTest.java
+++ src/test/java/com/enviro/assessment/grad001/mondeanna/recycling/integration/RecyclingTipIntegrationTest.java
@@ -1,29 +1,47 @@
 package com.enviro.assessment.grad001.mondeanna.recycling.integration;
 
+import com.enviro.assessment.grad001.mondeanna.recycling.RecyclingTip;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @AutoConfigureMockMvc
 @SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
 @DirtiesContext( classMode = ClassMode.BEFORE_EACH_TEST_METHOD )
 public class RecyclingTipIntegrationTest {
 
-    /* temporarily unused
+    private final String json = "{'tip': 'Compost Waste', 'description': 'Create own composite'}";
+    private final String requestMapping = "/api/v1/recycling/tips";
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    private final String requestMapping = "/api/v1/recycling-tips";
-
     @Test
     @Disabled( "response is status code 400 instead of 201" )
     public void testIntegrationOfSaveUsingJson() throws Exception {
-        String json = "{'name': 'Type 10', 'description': 'fancy test description'}";
-
         MockHttpServletResponse response = mockMvc.perform( post( requestMapping + "/" )
                         .accept( MediaType.APPLICATION_JSON )
                         .contentType( MediaType.APPLICATION_JSON )
@@ -36,15 +54,12 @@ public class RecyclingTipIntegrationTest {
 
         RecyclingTip typeTen = deserializeRecyclingTip( response );
 
-        assertThat( response.getStatus() ).isEqualTo( HttpStatus.CREATED.value() );
-        assertThat( typeTen.getName() ).isEqualTo( "Type 10" );
-        assertThat( typeTen.getDescription() ).isEqualTo( "fancy test description" );
+        assertThat( typeTen.getTip() ).isEqualTo( "Compost Waste" );
+        assertThat( typeTen.getDescription() ).isEqualTo( "Create own composite" );
     }
 
     @Test
     public void testIntegrationOfSaveWithInvalidArgs() throws Exception {
-        String json = "{'id': 10, 'name': 'Type 10', 'description': 'description'}";
-
         mockMvc.perform( post( requestMapping + "/" )
                         .accept( MediaType.APPLICATION_JSON )
                         .contentType( MediaType.APPLICATION_JSON )
@@ -54,32 +69,26 @@ public class RecyclingTipIntegrationTest {
     }
 
     @Test
-    @Disabled
     public void testIntegrationOfFindAll() throws Exception {
         MockHttpServletResponse response = mockMvc.perform( get( requestMapping + "/" ))
                 .andExpect( status().isOk() )
                 .andReturn().getResponse();
 
         List<RecyclingTip> responseRepository = deserializeRepository( response );
-        String expectedDescription = "(LCT1 < LC <= LCT2 or TC <= TCT1)";
-
-        assertThat( responseRepository.get( 0 ).getId() ).isEqualTo( 1 );
-        assertThat( responseRepository.get( 1 ).getName() ).isEqualTo( "Type 1" );
-        assertThat( responseRepository.get( 2 ).getDescription() ).contains( expectedDescription );
+        assertThat( responseRepository.isEmpty() ).isFalse();
     }
 
     @Test
-    @Disabled
     public void testIntegrationOfFindById() throws Exception {
-        MockHttpServletResponse response = mockMvc.perform( get( requestMapping + "/4" ))
+        MockHttpServletResponse response = mockMvc.perform( get( requestMapping + "/1" ))
                 .andExpect( status().isOk() )
                 .andReturn().getResponse();
 
         RecyclingTip category = deserializeRecyclingTip( response );
-        String expectedDescription = "(LCT0 < LC <= LCT1 or TC <= TCT1)";
+        String expectedDescription = "Be sure to sort your waste";
 
-        assertThat( category.getId() ).isEqualTo( 4 );
-        assertThat( category.getName() ).isEqualTo( "Type 3");
+        /* be aware that the test touches the actual database */
+        assertThat( category.getTip() ).isEqualTo( "Sort Your Rubbish" );
         assertThat( category.getDescription() ).contains( expectedDescription );
     }
 
@@ -107,10 +116,10 @@ public class RecyclingTipIntegrationTest {
         RecyclingTip category = deserializeRepository( findAllResponse ).get( 0 );
         long id = category.getId();
 
-        assertThat( category.getId() ).isEqualTo( id );
-        assertThat( category.getName() ).isEqualTo( "Type 0" );
+        assertThat( category.getId() ).isEqualTo( 1 );
+        assertThat( category.getTip() ).isEqualTo( "Sort Your Rubbish" );
 
-        String json = "{'name': 'Type 10', 'description': 'updated description'}";
+        String json = "{'tip': 'updated tip', 'description': 'updated description'}";
 
         MockHttpServletResponse response = mockMvc.perform( put( requestMapping + "/" + id )
                         .accept( MediaType.APPLICATION_JSON )
@@ -124,8 +133,8 @@ public class RecyclingTipIntegrationTest {
 
         RecyclingTip typeTen = deserializeRecyclingTip( response );
 
-        assertThat( typeTen.getName() ).isEqualTo( "Type 10" );
-        assertThat( typeTen.getDescription() ).isEqualTo( "fancy test description" );
+        assertThat( typeTen.getTip() ).isEqualTo( "updated tip" );
+        assertThat( typeTen.getDescription() ).isEqualTo( "updated description" );
     }
 
     @Test
@@ -136,8 +145,6 @@ public class RecyclingTipIntegrationTest {
 
         List<RecyclingTip> responseRepository = deserializeRepository( response );
         int invalidId = responseRepository.size() + 1;
-
-        String json = "{'name': 'Type 10', 'description': 'updated description'}";
 
         mockMvc.perform( put( requestMapping + "/" + invalidId )
                         .accept( MediaType.APPLICATION_JSON )
@@ -155,7 +162,7 @@ public class RecyclingTipIntegrationTest {
         long firstRepoSize = deserializeRepository( firstResponse ).size();
         long validId = firstRepoSize == 0 ? -1 :firstRepoSize - 1;
 
-        // presently unable to guarantee state of repository
+        /* presently unable to guarantee state of repository */
         if ( validId >= 0 ) {
             mockMvc.perform( delete( requestMapping + "/" + validId ))
                 .andExpect( status().isOk() );
@@ -195,5 +202,4 @@ public class RecyclingTipIntegrationTest {
             throws UnsupportedEncodingException, JsonProcessingException {
         return objectMapper.readValue( response.getContentAsString(), RecyclingTip.class );
     }
-    */
 }
